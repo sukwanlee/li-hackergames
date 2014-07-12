f1de96833821bddb31e733031f48e2d0e22a3859
Sukwan Lee <lee.sukwan@gmail.com>
2014-07-12 03:58:21 -0700

    preliminary work on dashboard page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/dashboard', function(req,res) {
+	res.render('dashboard');
+});
+
 module.exports = router;
